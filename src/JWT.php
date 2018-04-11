34d4a6c0b0b778eb2e2153c8bfafe155497fcd17
George Stan <contact@georgestan.com>
2018-04-11 19:06:06 +0300

    rewrite

--- src/JWT.php
+++ src/JWT.php
@@ -4,7 +4,6 @@ namespace iMemento\JWT;
 
 use Firebase\JWT\JWT as FirebaseJWT;
 use iMemento\Exceptions\InvalidTokenException;
-use iMemento\JWT\Exceptions\TokenNotDecodedException;
 
 /**
  * Class JWT
@@ -13,122 +12,47 @@ use iMemento\JWT\Exceptions\TokenNotDecodedException;
  */
 class JWT
 {
-
-    /**
-     * @var
-     */
-    protected $jwt;
-
-    /**
-     * @var
-     */
-    protected $issuer;
-
-    /**
-     * @var
-     */
-    protected $payload;
-
-
-    /**
-     * JWT constructor.
-     *
-     * @param $jwt
-     */
-    public function __construct($jwt)
-    {
-        $this->jwt = $jwt;
-    }
-
     /**
      * @param $leeway
      * @return $this
      */
-    public function setLeeway($leeway)
+    public static function setLeeway($leeway)
     {
         FirebaseJWT::$leeway = $leeway;
-
-        return $this;
     }
 
     /**
+     * @param string $jwt
      * @return mixed
      * @throws InvalidTokenException
      */
-    public function getIssuer()
+    public static function getPayload(string $jwt)
     {
-        //if already decoded, just return it
-        if(!is_null($this->issuer))
-            return $this->issuer;
-
-        $payload = $this->getPayload();
-
-        if(empty($payload->iss)) {
-            throw new InvalidTokenException('Issuer not set.');
-        }
-
-        $this->issuer = $payload->iss;
-
-        return $this->issuer;
-    }
-
-    /**
-     * @param string $key
-     * @return mixed
-     * @throws \Exception
-     */
-    public function get(string $key)
-    {
-        $payload = (array) $this->payload;
-
-        if(isset($payload[$key])) {
-            return $payload[$key];
-        }
-
-        if(empty($payload)) {
-            throw new TokenNotDecodedException('Token is not decoded yet.');
-        }
-
-        throw new \UnexpectedValueException('Key not in payload.');
-    }
-
-    /**
-     * @return mixed
-     * @throws InvalidTokenException
-     */
-    public function getPayload()
-    {
-        if(empty($this->jwt)) {
+        if(empty($jwt))
             throw new InvalidTokenException('The token is missing or empty.');
-        }
 
         //get the issuer from payload without verifying the signature and the timestamps
-        $tks = explode('.', $this->jwt);
+        $tks = explode('.', $jwt);
 
-        if(count($tks) !== 3) {
+        if(count($tks) !== 3)
             throw new InvalidTokenException('Wrong number of segments.');
-        }
 
         $bodyb64 = $tks[1];
 
-        if(null === $payload = FirebaseJWT::jsonDecode(FirebaseJWT::urlsafeB64Decode($bodyb64))) {
+        if(null === $payload = FirebaseJWT::jsonDecode(FirebaseJWT::urlsafeB64Decode($bodyb64)))
             throw new InvalidTokenException('Invalid claims encoding.');
-        }
 
-        $this->payload = $payload;
-
-        return $this->payload;
+        return $payload;
     }
 
     /**
+     * @param string $jwt
      * @param        $publicKey
      * @return mixed
      */
-    public function decode($publicKey)
+    public static function decode(string $jwt, $publicKey)
     {
-        $this->payload = FirebaseJWT::decode($this->jwt, $publicKey, ['RS256']);
-
-        return $this->payload;
+        return FirebaseJWT::decode($jwt, $publicKey, ['RS256']);
     }
 
     /**
@@ -136,7 +60,7 @@ class JWT
      * @param        $privateKey
      * @return mixed
      */
-    public static function encode($payload, $privateKey)
+    public static function encode(array $payload, $privateKey)
     {
         return FirebaseJWT::encode($payload, $privateKey, 'RS256');
     }
--- tests/JWTTest.php
+++ tests/JWTTest.php
@@ -2,7 +2,6 @@
 
 use PHPUnit\Framework\TestCase;
 use iMemento\Exceptions\InvalidTokenException;
-use iMemento\JWT\Exceptions\TokenNotDecodedException;
 use iMemento\JWT\JWT;
 
 /**
@@ -11,28 +10,6 @@ use iMemento\JWT\JWT;
 final class JWTTest extends TestCase
 {
 
-    public function testCanBeCreatedFromValidToken()
-    {
-        $this->assertInstanceOf(
-            JWT::class,
-            new JWT('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJhdXRoIiwibmFtZSI6Ikdlb3JnZSIsImF1ZCI6ImV4YW1wbGUuY29tIiwiaWF0IjoxMzU2OTk5NTI0LCJuYmYiOjEzNTcwMDAwMDB9.YuCVuC6JA4RPedAfH6bt3zTP0iB7hwMdDbgsWA9z8AM4HEcOpzE0OghcTlo6xuS4FcifI9fl_rfZQjCtDh7p2LUxn0vdKT0oFlecaDRxu8PEx9CV8zS6h7oYKE6p3gMx0MMIzNTWIY7jOKBLSQInygA3Jv_xXAH22zoUaif10Xg')
-        );
-    }
 
-    public function testCannotBeCreatedFromInvalidToken()
-    {
-        $this->expectException(InvalidTokenException::class);
-
-        $jwt = new JWT('asd.YuCVuC6JA4RPedAfH6bt3zTP0iB7hwMdDbgsWA9z8AM4HEcOpzE0OghcTlo6xuS4FcifI9fl_rfZQjCtDh7p2LUxn0vdKT0oFlecaDRxu8PEx9CV8zS6h7oYKE6p3gMx0MMIzNTWIY7jOKBLSQInygA3Jv_xXAH22zoUaif10Xg');
-        $jwt->getIssuer();
-    }
-
-    public function testTokenNotDecodedException()
-    {
-        $this->expectException(TokenNotDecodedException::class);
-
-        $jwt = new JWT('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJhdXRoIiwibmFtZSI6Ikdlb3JnZSIsImF1ZCI6ImV4YW1wbGUuY29tIiwiaWF0IjoxMzU2OTk5NTI0LCJuYmYiOjEzNTcwMDAwMDB9.YuCVuC6JA4RPedAfH6bt3zTP0iB7hwMdDbgsWA9z8AM4HEcOpzE0OghcTlo6xuS4FcifI9fl_rfZQjCtDh7p2LUxn0vdKT0oFlecaDRxu8PEx9CV8zS6h7oYKE6p3gMx0MMIzNTWIY7jOKBLSQInygA3Jv_xXAH22zoUaif10Xg');
-        $jwt->get('name');
-    }
 
 }
